074038cb530898a26d0310e853b142381b083cfa
980pran <shuklanavin760@gmail.com>
2024-05-23 15:37:58 +0530

    first commit

---
+++ firsr.js
@@ -0,0 +1,63 @@
+// let x = document.getElementById('navbar-text');
+// let p = document.getElementById('navbar');
+// let menuIcon = document.querySelector('.menuIcon');
+
+// function openMenu() {
+//     if (x.style.display === 'none') {
+//         x.style.display = 'block';
+//     }
+//     else {
+//         x.style.display = 'none';
+//     }
+// };
+// menuIcon.addEventListener('click', () => openMenu());
+
+// $(document).ready(function () {
+//     // Initialize Owl Carousel for the home banner slider
+//     $('.owl-carousel').owlCarousel({
+//         loop: true,
+//         margin: 0,
+//         nav: true, // Show navigation arrows
+//         autoplay: true, // Autoplay the slider
+//         autoplayTimeout: 5000, // Autoplay interval in milliseconds (5 seconds)
+//         autoplayHoverPause: true, // Pause autoplay on mouse hover
+//         responsive: {
+//             0: {
+//                 items: 1 // Number of items to display on mobile devices
+//             },
+//             600: {
+//                 items: 1 // Number of items to display on tablets
+//             },
+//             1000: {
+//                 items: 1 // Number of items to display on desktops
+//             }
+//         }
+//     });
+// });
+
+// $(document).ready(function () {
+//     $('.owl-carousel').owlCarousel({
+//         loop: true,
+//         margin: 10,
+//         nav: true,
+//         responsive: {
+//             0: {
+//                 items: 1
+//             },
+//             600: {
+//                 items: 1
+//             },
+//             1000: {
+//                 items: 1
+//             }
+//         }
+//     })
+// });
+
+// let u = document.getElementById("pop-up");
+// function openfun() {
+//     u.style.display = "block"
+// }
+// function closefun() {
+//     u.style.display = "none";
+// }
